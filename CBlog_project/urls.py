702fc3d0db54e2986b85f292c9ab8c8dda3a2acd
ckp7blessed <78719243+ckp7blessed@users.noreply.github.com>
2022-09-20 00:42:37 -0700

    cleaned up urls.py

--- CBlog_project/urls.py
+++ CBlog_project/urls.py
@@ -1,18 +1,3 @@
-"""CBlog_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -27,7 +12,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.UserProfileListView.as_view(), name='profile'),
-    #path('search/', user_views.UserResultsView.as_view(), name='user_results'),
     path('profile/settings', user_views.profile_settings, name='profile_settings'),
     path('profile/followers', user_views.FollowersListView.as_view(), name='followers'),
     path('profile/following', user_views.FollowingListView.as_view(), name='following'),
