45ba68dc2757d1011d3a9582a10f1e8c22f98843
ckp7blessed <78719243+ckp7blessed@users.noreply.github.com>
2022-09-20 00:44:35 -0700

    cleaned up admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from . models import Post, PostImage, Category, Like, Comment, CommentLike, Notification, ThreadModel
-# Register your models here.
 
 class ImageInline(admin.TabularInline):
 	model = PostImage
