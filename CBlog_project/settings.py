ac8396133ff66614b74b65e53fee1b4165a1d2d0
ckp7blessed <78719243+ckp7blessed@users.noreply.github.com>
2022-10-14 00:56:11 -0700

    update email address for django backends

--- CBlog_project/settings.py
+++ CBlog_project/settings.py
@@ -155,8 +155,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('BTA_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('BTA_EMAIL_PASS')
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
