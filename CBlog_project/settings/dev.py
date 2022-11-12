646ea7b0a04249adba3f78573e6cffdb7b634bb8
ckp7blessed <78719243+ckp7blessed@users.noreply.github.com>
2022-11-11 23:06:41 -0800

    testing settings..

--- CBlog_project/settings/dev.py
+++ CBlog_project/settings/dev.py
@@ -10,8 +10,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
-# DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*', 'bytetheapple.herokuapp.com']
 
