93596229b978265d12bb1e2cb6d79bba89bfe7f1
ckp7blessed <78719243+ckp7blessed@users.noreply.github.com>
2022-11-11 22:32:28 -0800

    update debug_value uc

--- CBlog_project/settings/dev.py
+++ CBlog_project/settings/dev.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
 ALLOWED_HOSTS = ['*', 'bytetheapple.herokuapp.com']
 
--- CBlog_project/settings/prod.py
+++ CBlog_project/settings/prod.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
 ALLOWED_HOSTS = ['.bytetheapples.com', 'bytetheapple.herokuapp.com']
 
