bb33f7b2ecf4d1b74fe387cf2356e23e59537116
ckp7blessed <78719243+ckp7blessed@users.noreply.github.com>
2022-11-14 23:21:14 -0800

    static images serving from S3

--- CBlog_project/settings/dev.py
+++ CBlog_project/settings/dev.py
@@ -9,7 +9,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.bytetheapples.com', 'bytetheapple.herokuapp.com']
 
